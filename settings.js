7f3d69619af9da6e130822c1d25f4f6ddd3a05d2
nicholasalmendro101 <nicholasalmendro101@gmail.com>
2026-03-26 14:15:19 +0200

    Update settings.js for 1hplowkey

--- settings.js
+++ settings.js
@@ -1,23 +1,6 @@
-import 'dotenv/config';
-
-const settings = {
-/* ================= BOT IDENTITY ================= */
-
-botName: process.env.BOT_NAME || 'SUBZERO-MD',
-botOwner: process.env.BOT_OWNER || 'Darrell M',
-ownerNumber: process.env.OWNER_NUMBER || '263719647303',
-author: process.env.AUTHOR || 'Mr Frank OFC',
-packname: process.env.PACKNAME || 'SUBZERO-MD',
-description: process.env.DESCRIPTION || 'Multi-device WhatsApp bot',
-
-/* ================= SESSION ================= */
-
-sessionId: process.env.SESSION_ID || 'Ice~WmdYGf',
-pairingNumber: process.env.PAIRING_NUMBER || '',
-
-/* ================= CDN ================= */
-
-CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+module.exports = {
+  SESSION_ID: "Ice~89b410",
+  OWNER_NUMBER: "+27 63 312 2146", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-export default settings;
